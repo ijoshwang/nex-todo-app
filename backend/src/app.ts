c1900cd0eacd4fecdce5e5e2e2f84cb1296314a4
Josh <ijoshwang@gmail.com>
2024-07-19 22:50:17 +0800

    feat: add test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import dutyRoutes from './routes/duty.routes'
+import errorHandler from './middlewares/errorHandler'
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use('/duties', dutyRoutes)
+app.use(errorHandler) // Add the error-handling middleware
 
 export default app
