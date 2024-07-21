a572869d6c60e7e4df9759eb71c1b6663d829802
Josh <ijoshwang@gmail.com>
2024-07-21 14:41:11 +0800

    feat: add basic validation

--- frontend/src/components/EditItem.tsx
+++ frontend/src/components/EditItem.tsx
@@ -1,5 +1,10 @@
-import { useState } from 'react'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import { Button, Input } from 'antd'
+
+type Inputs = {
+  itemName: string
+}
+
 interface AddItemProps {
   itemName?: string
   onHandleSave: (itemName: string) => void
@@ -11,31 +16,70 @@ export default function EditItem({
   onHandleSave,
   onHandleCancel,
 }: AddItemProps) {
-  const [value, setValue] = useState(itemName)
-
-  const onSave = () => {
-    onHandleSave(value)
-  }
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      itemName: itemName,
+    },
+  })
 
   const onCancel = () => {
     onHandleCancel()
   }
 
+  const onSave: SubmitHandler<Inputs> = (data) => {
+    const { itemName } = data
+
+    if (itemName === '') {
+      onCancel()
+    } else {
+      onHandleSave(itemName)
+    }
+  }
+
   return (
     <div className="edit-item">
-      <Input
+      {/* <Input
         className="edit-input"
         variant="borderless"
         autoFocus
         value={value}
         placeholder="Add a new item"
         onChange={(e) => setValue(e.target.value)}
-        onPressEnter={onSave}
+        onPressEnter={handleSubmit(onSave)}
         onBlur={onCancel}
+        {...register('item')}
+        required
+      /> */}
+      <Controller
+        name="itemName"
+        control={control}
+        rules={{
+          maxLength: 10,
+        }}
+        render={({ field }) => (
+          <Input
+            {...field}
+            className="edit-input"
+            variant="borderless"
+            autoFocus
+            placeholder="Add a new item"
+            onPressEnter={handleSubmit(onSave)}
+            onBlur={onCancel}
+          />
+        )}
       />
-      <Button type="primary" onClick={onSave}>
+
+      <Button type="primary" onClick={handleSubmit(onSave)}>
         Save
       </Button>
+
+      {errors.itemName && (
+        <span className="error-message">Name cannot exceed 100 characters</span>
+      )}
     </div>
   )
 }
--- frontend/src/components/Todos.tsx
+++ frontend/src/components/Todos.tsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
+import { useEffect, useState } from 'react'
 import { Spin } from 'antd'
-import * as yup from 'yup'
 
 import { Duty, getDuties } from '../services/duty'
 
@@ -11,22 +8,7 @@ import Item from './Item'
 
 import './Todos.css'
 
-const dutySchema = yup.object().shape({
-  name: yup
-    .string()
-    .required('Name is required')
-    .max(100, 'Name cannot exceed 100 characters'),
-})
-
-type FormData = {
-  name: string
-}
-
 export default function Todos() {
-  const { control, handleSubmit, reset, setValue } = useForm<FormData>({
-    resolver: yupResolver(dutySchema),
-  })
-
   const [items, setItems] = useState<Duty[]>([])
   const [loading, setLoading] = useState<boolean>(true)
   const [error, setError] = useState<string | null>(null)
