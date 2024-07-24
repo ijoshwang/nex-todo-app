e166730c5cb34ee379a71af8b7fa443e8f22a5ad
Josh <ijoshwang@gmail.com>
2024-07-24 10:12:22 +0800

    feat: start back and front

--- backend/src/database/setup.ts
+++ backend/src/database/setup.ts
@@ -3,16 +3,25 @@ import { queries } from './queries'
 import { seedDuties } from './seed'
 
 const seedData = async () => {
-  for (const duty of seedDuties) {
-    await pool.query(queries.insertDuty, [duty.name])
-  }
+  // Check if the duties table is empty
+  const result = await pool.query(queries.selectAllDuties)
 
-  console.log('Database seeded!')
+  if (result.rows.length === 0) {
+    // If the table is empty, insert seed data
+    for (const duty of seedDuties) {
+      await pool.query(queries.insertDuty, [duty.name])
+    }
+
+    console.log('Database seeded!')
+  } else {
+    console.log('Duties table already contains data. Skipping seeding.')
+  }
 }
 
 const setupDb = async () => {
   try {
-    // await pool.query('DROP TABLE IF EXISTS duties');
+    // await pool.query(queries.dropTable)
+    // console.log('Table droped!')
     await pool.query(queries.createTable)
     console.log('Table created!')
     await seedData()
